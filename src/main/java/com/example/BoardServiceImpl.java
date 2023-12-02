0ef8974123857cc8efc4cd4751c1788f05a9b8e7
KimDuHwan <hguflamejohn@gmail.com>
2023-12-02 22:43:27 +0900

    BoardDAO make2

---
+++ src/main/java/com/example/BoardServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.List;
+
+@Service
+public class BoardServiceImpl {
+    BoardDAO boardDAO;
+    public int insertBoard(BoardVO vo){
+        return boardDAO.insertBoard(vo);
+    }
+    public int deleteBoard(int seq){
+        return boardDAO.deleteBoard(seq);
+    }
+    public int updateBoard(BoardVO vo){
+        return boardDAO.updateBoard(vo);
+    }
+    public BoardVO getBoard(int seq){
+        return boardDAO.getBoard(seq);
+    }
+    public List<BoardVO> getBoardList(){
+        return boardDAO.getBoardList();
+    }
+}
